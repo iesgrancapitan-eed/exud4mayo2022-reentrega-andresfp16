f09586ec733e277904fd1ac7420252e77b5ce693
andresfp16 <a21romaan@iesgrancapitan.org>
2022-05-20 16:54:52 +0200

    refactor: GUAUUUUUUUUUU

--- src/com/ARM/eed/examen/OptimizacionYDocumentacion/Perro.java
+++ src/com/ARM/eed/examen/OptimizacionYDocumentacion/Perro.java
@@ -1,6 +1,7 @@
 package com.ARM.eed.examen.OptimizacionYDocumentacion;
 
 public class Perro {
+	private static final String GUAUUUUUUUUUU = "GUAUUUUUUUUUU";
 	private int age;
 	private String name;
 
@@ -20,7 +21,7 @@ public class Perro {
 	}
 
 	public String ladrar() {
-		return "GUAUUUUUUUUUU";
+		return GUAUUUUUUUUUU;
 	}
 
 	public String getName() {
