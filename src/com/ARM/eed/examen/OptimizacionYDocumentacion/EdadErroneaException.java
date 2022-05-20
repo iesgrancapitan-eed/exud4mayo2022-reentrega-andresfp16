22324779056b0480902bf4a44200183f1b6c06d7
andresfp16 <a21romaan@iesgrancapitan.org>
2022-05-20 16:48:26 +0200

    closes #1

---
+++ src/com/ARM/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
@@ -0,0 +1,8 @@
+package com.ARM.eed.examen.OptimizacionYDocumentacion;
+
+public class EdadErroneaException extends Exception {
+
+	public EdadErroneaException(String string) {
+		super(string);
+	}
+}
